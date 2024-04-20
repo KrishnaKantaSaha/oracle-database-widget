37a09465b60a5aff69a5d1955c2c904f7be9ce70
KrishnaKantaSaha <i.krishna100@gmail.com>
2024-04-20 09:30:56 +0100

    Validation SQL changes

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
-This project is about a record-keeping database solution for Widget Ltd. for modernization of their currently manual-filing system to Oracle DB system. 
+This project is about a record-keeping database solution for Widget Ltd. for modernization of their currently manual-filing system to Oracle DB system.
+ 
 Below are the schemas:
 1. WIDGET 		: Main schema containing all the objects
 2. WIDGET_RO 	: This no-object schema/user has read-only access on WIDGET objects
 3. WIDGET_RW 	: This no-object schema/user has read-write access on WIDGET objects
 
 The deployment will happen through RLM and Jenkins build
-Use file 'deployment_steps.ctl' to deploy the widget's database objects
+
+Note: Use file 'deployment_steps.ctl' to deploy the widget's database objects
--- database/validation.sql
+++ database/validation.sql
@@ -21,9 +21,13 @@ select 9 row#, 'TRIGGER' OBJECT_TYPE, 'TRG_TEMP_EMP_MASTER_AUDIT_INS_UDT' OBJECT
 select 10 row#, 'TRIGGER' OBJECT_TYPE, 'TRG_TEMP_DEPT_MASTER_AUDIT_INS_UDT' OBJECT_NAME from dual union 
 select 11 row#, 'TYPE' OBJECT_TYPE, 'TEMP_EMP_TBL_TYP' OBJECT_NAME from dual union 
 select 12 row#, 'TYPE' OBJECT_TYPE, 'TEMP_EMP_REC_TYP' OBJECT_NAME from dual union 
+select 13 row#, 'PACKAGE' OBJECT_TYPE, 'PKG_OPERATIONS' OBJECT_NAME from dual union 
+select 14 row#, 'PACKAGE BODY' OBJECT_TYPE, 'PKG_OPERATIONS' OBJECT_NAME from dual union 
+select 15 row#, 'FUNCTION' OBJECT_TYPE, 'SHOW_SALARY' OBJECT_NAME from dual union 
+select 16 row#, 'PROCEDURE' OBJECT_TYPE, 'GET_DEPARTMENT_WISE_EMPLOYEES' OBJECT_NAME from dual union 
 select 1 row#, 'TABLE' OBJECT_TYPE, 'TEMP_EMP_MASTER' OBJECT_NAME from dual where 1=2 
 ) 
-select 
+select /*csv*/
 sys_context('USERENV', 'DB_NAME')||'-'||sys_context('USERENV', 'CURRENT_SCHEMA') db_schema,sysdate, 
 master.OBJECT_TYPE 
 ,COUNT(master.OBJECT_NAME) CNT_TOTAL_OBJECT 
@@ -33,22 +37,22 @@ from master_objects_data master left join dba_objects local
 ON (master.OBJECT_TYPE=local.OBJECT_TYPE and local.OWNER='WIDGET' and master.OBJECT_NAME=local.OBJECT_NAME) 
 --WHERE local.OBJECT_NAME is NULL 
 group by master.OBJECT_TYPE 
-order by CNT_OBJECT_MISSING desc, decode(OBJECT_TYPE,'TABLE',1,'VIEW',2,'INDEX',3,'SEQUENCE',4,'MATERIALIZED VIEW',5,'TRIGGER',6,'PACKAGE',7,'PACKAGE BODY',8,'PROCEDURE',9,'FUNCTION',10,11) 
+order by CNT_OBJECT_MISSING desc, decode(OBJECT_TYPE,'TABLE',1,'INDEX',2,'SEQUENCE',3,'TRIGGER',5,'PACKAGE',6,'PACKAGE BODY',7,'PROCEDURE',8,'FUNCTION',9,10);
 
 
 /**************************************************************************************************
 --Count Validation
 **************************************************************************************************/
 
-select sys_context('USERENV', 'DB_NAME')||'-'||sys_context('USERENV', 'CURRENT_SCHEMA') db_schema,sysdate,decode(cnt,10,'SUCCESS: all '||cnt||' present','FAILURE: '||cnt||' present') status 
+select /*csv*/ sys_context('USERENV', 'DB_NAME')||'-'||sys_context('USERENV', 'CURRENT_SCHEMA') db_schema,sysdate,decode(cnt,10,'SUCCESS: all '||cnt||' present','FAILURE: '||cnt||' present') status 
 from (select count(1) cnt from TEMP_EMP_MASTER);
 
-select sys_context('USERENV', 'DB_NAME')||'-'||sys_context('USERENV', 'CURRENT_SCHEMA') db_schema,sysdate,decode(cnt,4,'SUCCESS: all '||cnt||' present','FAILURE: '||cnt||' present') status 
+select /*csv*/ sys_context('USERENV', 'DB_NAME')||'-'||sys_context('USERENV', 'CURRENT_SCHEMA') db_schema,sysdate,decode(cnt,4,'SUCCESS: all '||cnt||' present','FAILURE: '||cnt||' present') status 
 from (select count(1) cnt from TEMP_DEPT_MASTER);
 
-select sys_context('USERENV', 'DB_NAME')||'-'||sys_context('USERENV', 'CURRENT_SCHEMA') db_schema,sysdate,decode(cnt,10,'SUCCESS: all '||cnt||' present','FAILURE: '||cnt||' present') status 
+select /*csv*/ sys_context('USERENV', 'DB_NAME')||'-'||sys_context('USERENV', 'CURRENT_SCHEMA') db_schema,sysdate,decode(cnt,10,'SUCCESS: all '||cnt||' present','FAILURE: '||cnt||' present') status 
 from (select count(1) cnt from TEMP_EMP_MASTER_AUDIT);
 
-select sys_context('USERENV', 'DB_NAME')||'-'||sys_context('USERENV', 'CURRENT_SCHEMA') db_schema,sysdate,decode(cnt,4,'SUCCESS: all '||cnt||' present','FAILURE: '||cnt||' present') status 
+select /*csv*/ sys_context('USERENV', 'DB_NAME')||'-'||sys_context('USERENV', 'CURRENT_SCHEMA') db_schema,sysdate,decode(cnt,4,'SUCCESS: all '||cnt||' present','FAILURE: '||cnt||' present') status 
 from (select count(1) cnt from TEMP_DEPT_MASTER_AUDIT);
 
